14acf5286ea81e28b5473b7301e6851b2449e567
dilshodturabov <avlomberdioglid@gmail.com>
2023-09-20 00:01:02 +0500

    xatolarni winston-mongtodb orqali mongodbga yozish yo'lga qo'yildi

--- index.js
+++ index.js
@@ -11,15 +11,22 @@ const errorMiddleware = require('./middleware/error');
 const mongoose = require('mongoose');
 const config = require('config');
 const winston = require('winston');
+require('winston-mongodb');
 
-winston.add(new winston.transports.File({filename: 'restfullapp-logs.log'}));
-if(!config.get('jwtPrivateKey')){
-    console.error('CRITICAL ERROR: restfullapp_jwtPrivateKey environment variable is undefined!');
+
+winston.add(new winston.transports.Console());
+winston.add(new winston.transports.File({ filename: 'logs/rf-logs.log' }));
+winston.add(new winston.transports.MongoDB({ db: 'mongodb://127.0.0.1:27017/test-logs' }));
+
+
+
+if (!config.get('jwtPrivateKey')) {
+    winston.error('CRITICAL ERROR: restfullapp_jwtPrivateKey environment variable is undefined!');
     process.exit(1);
 }
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use('/api/categories', categoriesRoute);
 app.use('/api/customers', customersRoute);
 app.use('/api/courses', coursesRoute);
@@ -27,15 +34,17 @@ app.use('/api/enrollments', enrollmentsRoute);
 app.use('/api/users', usersRoute);
 app.use('/api/auth', authRoute);
 app.use(errorMiddleware);
+
+
 const urlToDatabase = "mongodb://127.0.0.1:27017/test";
-mongoose.connect(urlToDatabase, {useNewUrlParser:true, useUnifiedTopology:true})
-    .then(()=> console.log('database connected...'))
-    .catch(err=>console.log('Program crashed on connecting to database!', err))
+mongoose.connect(urlToDatabase, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => winston.debug('database connected...'))
+    .catch(err => winston.error('Program crashed on connecting to database!', err))
 
 // console.log(config.get('mailserver.password'));
 
 const port = process.env.PORT || 5000;
 
-app.listen(port,()=>{
-    console.log(`${port} - portni eshitayapman...`);
+app.listen(port, () => {
+    winston.info(`${port} - portni eshitayapman...`);
 });
